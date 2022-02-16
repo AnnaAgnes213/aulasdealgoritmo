9a7f02d9cc6ca282b828c21f027dd94f325d5cfe
Anna Lara Ferreira <66583737+AnnaLara213@users.noreply.github.com>
2022-02-16 11:12:39 -0300

    Add files via upload

--- exe1002.cpp
+++ exe1002.cpp
@@ -1,12 +1,16 @@
 /*A fórmula para calcular a área de uma circunferência é: area = p . raio2. Considerando para este problema que p = 3.14159:*/
 #include <stdio.h>
+#include <math.h>
+
+#define PI 3.14159 /*Costantes são escritas em letras maiusculas.
+Constantes não mudam seu valor durante o código.*/
 
 
 int main (){
 	double n, r;
 	printf("Digite o raio: ");
 	scanf("%lf", &r);
-	n = 3.14159;
-	printf("AREA: %.4lf", n * (r*r) );
+	n = PI;
+	printf("AREA: %.4lf", n * pow(r,2) );
 	return 0;
 }
