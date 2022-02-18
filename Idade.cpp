5b4ddbb7dfff6277f0144395b9d5bb061dcfdd95
Anna Lara Ferreira <66583737+AnnaLara213@users.noreply.github.com>
2022-02-18 11:22:00 -0300

    Add files via upload

---
+++ Idade.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+ int main(){
+ 	int A;
+ 	printf("Digite a idade: ");
+ 	scanf("%d", &A);
+ 	
+ 	if (A <= 10){
+ 		printf("Crianca");
+	 }
+	 else if(A <= 18){
+	 	printf("Adolescente");
+	 }
+	  else if(A <= 60){
+	 	printf("Adulto");
+	 }
+	  else if(A <= 100){
+	 	printf("Idoso");
+	 }
+	  else if(A <= 150){
+	 	printf("Zumbi");
+	 }
+	 else printf("Vampiro");
+ }
